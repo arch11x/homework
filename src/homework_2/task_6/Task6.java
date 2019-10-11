37b661d74c765343d88dabcc5dfaee7cd337631e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-11 18:04:17 +0300

    Update Task6.java

--- src/homework_2/task_6/Task6.java
+++ src/homework_2/task_6/Task6.java
@@ -4,6 +4,7 @@ public class Task6 {
     public static void main(String[] args) {
         int i = 2;
         while (i <= 100) {
+            // лучше через if (i % 2 == 0)
             System.out.print(" "+i);
             i +=2;
         }
