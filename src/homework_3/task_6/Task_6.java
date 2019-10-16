b1f26d142b24c988116766ebf6b91274520c4d84
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:37:46 +0300

    Update Task_6.java

--- src/homework_3/task_6/Task_6.java
+++ src/homework_3/task_6/Task_6.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class Task_6 {
     public static void main(String[] args) {
         int[] array = {5, 6, 2, 7, 3, 4, 1, 0, 8, 5};
+        // arrayNew (плохое название) -> arrayWithEvenElements
         int[] arrayNew = new int[array.length];
         int value = 0;
-
+// пустая строка не нужа
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
@@ -15,11 +16,11 @@ public class Task_6 {
                 value++;
             }
         }
-
+// пустая строка не нужа
 
         int[] even = Arrays.copyOf(arrayNew, value);
         System.out.println(Arrays.toString(even));
-
-
+// пустая строка не нужа
+// пустая строка не нужа
     }
 }
