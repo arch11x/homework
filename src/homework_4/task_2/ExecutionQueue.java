48f48a2b04ca1612a2f744d311136b0d9a3aa210
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-20 10:19:41 +0300

    Update ExecutionQueue.java

--- src/homework_4/task_2/ExecutionQueue.java
+++ src/homework_4/task_2/ExecutionQueue.java
@@ -1,16 +1,16 @@
 package homework_4.task_2;
 
 public class ExecutionQueue {
-    private static String msg;
+    private static String msg; // нужен абзац
     static {
         msg = "static block";
-    }
+    } // нужен абзац
     {
         msg = "regular init block";
-    }
+    } // нужен абзац
     public ExecutionQueue() {
         msg = "constructor value";
-    }
+    } // нужен абзац
     public static void main(String[] args) {
         ExecutionQueue executionQueue = new ExecutionQueue();
 
