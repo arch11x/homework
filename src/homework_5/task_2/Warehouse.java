f9cac8a79029c34fa8b7486d70d704525ae6d65e
Artur Kuznetcov <arturkuznecov2015@gmail.com>
2019-10-22 17:53:16 +0300

    hm_5

---
+++ src/homework_5/task_2/Warehouse.java
@@ -0,0 +1,26 @@
+package homework_5.task_2;
+
+public class Warehouse {
+    private final String[] array = new String[4];
+
+    public Warehouse() {
+        array[0] = "shovel";
+        array[1] = "rake";
+        array[2] = "bucket";
+        array[3] = "Earth";
+    }
+
+    public void next(int next) {
+        if (next == array.length - 1) {
+            System.out.println("Следующий элемент: " + array[0].toString());
+        } else {
+            System.out.println("Следующий элемент: " + array[next].toString());
+        }
+    }
+
+    public void getArray() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(" Элемент массива: " + array[i].toString());
+        }
+    }
+}
