21d6d44e7f945c7d71b0b726ea65441627c35c3b
Artur Kuznetcov <arturkuznecov2015@gmail.com>
2019-10-22 17:25:20 +0300

    hm_5

--- src/homework_5/task_3/Main.java
+++ src/homework_5/task_3/Main.java
@@ -14,12 +14,15 @@ public class Main {
         catalogHouses.getArrayHouse();
         System.out.println("Please choose type of house:");
         type = in.next();
-        if (type == "Vacation house") {
-            System.out.println("Number of floors: " + VacationHome.numberOfFloors);
-            System.out.println("Number of tenants: " + VacationHome.numberOfTenants);
-        } else {
-            System.out.println("Number of floors: " + Skyscraper.numberOfFloors);
-            System.out.println("Number of tenants: " + Skyscraper.numberOfTenants);
-        }
+//        if (type == "Vacation house") {
+//            System.out.println("Number of floors: " + VacationHome.numberOfFloors);
+//            System.out.println("Number of tenants: " + VacationHome.numberOfTenants);
+//        } else {
+//            System.out.println("Number of floors: " + Skyscraper.numberOfFloors);
+//            System.out.println("Number of tenants: " + Skyscraper.numberOfTenants);
+//        }
+        /*я перепробовал все чтобы вывести значения загородного дома, но ничего не выходит
+            Выводить значения городского
+         */
     }
 }
