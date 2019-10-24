3c2a12c61c3cdbe93e79f63059b578d2c09d7c06
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 00:36:50 +0300

    Update Computer.java

--- src/homework_5/task_1/Computer.java
+++ src/homework_5/task_1/Computer.java
@@ -1,5 +1,6 @@
 package homework_5.task_1;
 
+// я уже рассказывал на лекции про использование дефолтных методов, коротко - старайся их не использовать
 public interface Computer {
     default void on() {
         System.out.println("Computer on");
