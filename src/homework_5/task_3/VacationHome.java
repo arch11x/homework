2df8b0d22e869101d0d1b84dc5704c9a647aef4d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 00:46:14 +0300

    Update VacationHome.java

--- src/homework_5/task_3/VacationHome.java
+++ src/homework_5/task_3/VacationHome.java
@@ -1,11 +1,15 @@
 package homework_5.task_3;
 
+// поля класса должны быть приватными
 public class VacationHome implements Interface {
 
+    // думаю это поле не должно быть статичным
     static int numberOfTenants;
+    // думаю это поле не должно быть статичным
     static int numberOfFloors;
     String type;
 
+    // сделай лучше конструктор с аргументами
     public VacationHome () {
         numberOfFloors = 3;
         numberOfTenants = 4;
@@ -30,6 +34,6 @@ public class VacationHome implements Interface {
         System.out.println("Number floors: "+ numberOfFloors);
     }
 
-
-
+// пустая строка не нужна
+// пустая строка не нужна
 }
