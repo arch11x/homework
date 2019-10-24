a2143f3c7ebae08644d588fecd68677ad1b140de
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 00:37:52 +0300

    Update Main.java

--- src/homework_5/task_1/Main.java
+++ src/homework_5/task_1/Main.java
@@ -2,6 +2,7 @@ package homework_5.task_1;
 
 import jdk.swing.interop.SwingInterOpUtils;
 
+// Main -> не лучшее название для класса, который имплементит интерфейс Computer, например, Laptop
 public class Main implements Computer {
     public void reset() {
         System.out.println("Computer reset and update" );
