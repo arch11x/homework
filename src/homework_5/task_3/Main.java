c72380418ecfcab54e602040407620fa0e694739
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 20:23:53 +0300

    Update Main.java

--- src/homework_5/task_3/Main.java
+++ src/homework_5/task_3/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main (String [] args) {
     //  я не очень понимаю, как дальше вывести с выбором типа дома
         // подойди ко мне с этим вопросом на занятии
+        
+        //House[] houses = new House[3];
+        // SkyHouse sky = new SkyHouse(...);
+        
+        // new Catalog(houses);
     }
 }
