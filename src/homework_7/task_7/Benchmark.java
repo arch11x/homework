4d16969da542168d31618beb4c2a97c409d8e8cf
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 20:35:10 +0300

    Update Benchmark.java

--- src/homework_7/task_7/Benchmark.java
+++ src/homework_7/task_7/Benchmark.java
@@ -3,5 +3,25 @@ package homework_7.task_7;
 public class Benchmark {
     public static void main(String[] args) {
         //  у меня не получилось написать Benchmark. Я подойду на уроке.
+        
+        /*
+        int numberOfIterations = 10_000;
+        String symbolToAdd = "A"; 
+        int start;
+        int stop;
+        
+        String string = "";
+        
+        start = System.currentTimeMillis();
+        for(int i = 0; i < numberOfIterations; i++) {
+          string = string + symbolToAdd;
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("Time took for string:" + (stop-start));
+        
+        и так же для StringBuilder и StrungBuffer
+
+        желательно вынести в отдельный метод, а не все писать в main()
+        */
     }
 }
