d88831c033b96bf9cef52e2005faf5012463143f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-01 15:42:31 +0300

    Update WashingMachine.java

--- src/homework_8/task_4/WashingMachine.java
+++ src/homework_8/task_4/WashingMachine.java
@@ -9,9 +9,9 @@ public class WashingMachine {
     private void work() {
         try {
             System.out.println("I'm working");
-            while (true) {
+            while (true) { // лучше завести boolean поле и на напрямую true
                 if (System.currentTimeMillis() % 2 == 1) {
-                    throw new MyException("Error 404;(");
+                    throw new MyException("Error 404;("); // 404 это код спец ошибки из http и используется для других целей 
                 }
             }
         } catch (MyException e) {
