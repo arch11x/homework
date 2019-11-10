a9aee89edd81824b9e42b8dfe3d039f32df8d000
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:57:32 +0300

    Update WriteSecretMessage.java

--- src/homework_9/task_6/WriteSecretMessage.java
+++ src/homework_9/task_6/WriteSecretMessage.java
@@ -10,6 +10,8 @@ public class WriteSecretMessage {
 
     public static void main(String[] args) {
         try (FileWriter fw = new FileWriter(FILE_PATH, true)) {
+            // перед тем, как записать в файл стоило узнать сколько сейчас в нем записано при помощи метода available()
+            // чтобы потом знать с какого места читать + надо узнать сколько байт в твоем сообщении, чтобы знать СКОЛЬКО прочитать
             fw.write("It can only be read by a select few");
 
             fw.flush();
