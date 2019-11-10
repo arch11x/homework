b588ae50dd84c35162074394d6a79392d89830d0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 14:56:12 +0300

    Update ReadSecretMessage.java

--- src/homework_9/task_6/ReadSecretMessage.java
+++ src/homework_9/task_6/ReadSecretMessage.java
@@ -11,6 +11,11 @@ public class ReadSecretMessage {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
+            /*
+            в этом коде ты прочитал весь файл, а нужно было только ту часть, где находится сообщение
+тебе нужно было запомнить место с которого начинается сообщение и сколько байт занимает сообщение, чтобы затем
+прочитать именно этот кусок файла
+            */
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
