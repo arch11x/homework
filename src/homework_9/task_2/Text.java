0c72d826cccc2dd054a19d21a24b3043dde6d875
Artur Kuznetcov <arturkuznecov2015@gmail.com>
2019-11-12 12:16:04 +0300

    hm_9

--- src/homework_9/task_2/Text.java
+++ src/homework_9/task_2/Text.java
@@ -3,18 +3,18 @@ package homework_9.task_2;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
+//+
 public class Text {
 
     private static final String FILE_PATH = "src/homework_9/task_2/output";
 
     public static void main(String[] args) {
         try (FileWriter fw = new FileWriter(FILE_PATH, true)) {
-            for (int i = 0; i < 10000; i++) { // 10_000 в переменную
+            int replay = 10000;
+            for (int i = 0; i < replay; i++) {
                 fw.write("My name is Artur =)");
                 fw.write("\n");
             }
-// пустая строка не нужна
             fw.flush();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
