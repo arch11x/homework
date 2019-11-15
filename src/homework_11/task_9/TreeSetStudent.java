e5fb9bbad6df20681f61b99cacd9007202c81d4e
Artur Kuznetcov <arturkuznecov2015@gmail.com>
2019-11-15 17:21:47 +0300

    hm_11

--- src/homework_11/task_9/TreeSetStudent.java
+++ src/homework_11/task_9/TreeSetStudent.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 public class TreeSetStudent {
     public static void main(String[] args) {
         Set<Student> students = new TreeSet<>();
-        students.add(new Student("Dima ", 16, "FFA"));
+        students.add(new Student("Dima", 16, "FFA"));
         students.add(new Student("Artur", 14, "FFA"));
         students.add(new Student("Liza", 20, "GTA"));
         students.add(new Student("Artur", 18, "LGA"));
