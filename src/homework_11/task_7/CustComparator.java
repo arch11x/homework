fd0108dfd7fd31ea8779fd59b93f0b2e5bc36552
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 21:35:23 +0300

    Update CustComparator.java

--- src/homework_11/task_7/CustComparator.java
+++ src/homework_11/task_7/CustComparator.java
@@ -2,6 +2,7 @@ package homework_11.task_7;
 
 import java.util.Comparator;
 
+// для стандартных типов в джава можно использовать готовый функционал в виде new TreeSet<>(Collections.reversedOrder());
 public class CustComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer intFirst, Integer intSecond) {
