30729283bc39ecdd8c4f434eff6c30cb31f64b93
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 21:36:00 +0300

    Update HashSetShop.java

--- src/homework_11/task_8/HashSetShop.java
+++ src/homework_11/task_8/HashSetShop.java
@@ -3,6 +3,7 @@ package homework_11.task_8;
 import java.util.HashSet;
 import java.util.Set;
 //не переопределили hashCode(),equals()
+// +
 public class HashSetShop {
     public static void main(String[] args) {
         Set<Shop> shops = new HashSet<>();
