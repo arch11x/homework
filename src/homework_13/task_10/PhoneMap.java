bb11a5dc8ebacfd33d33e6c281cfa3fccf65f099
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 21:10:26 +0300

    Update PhoneMap.java

--- src/homework_13/task_10/PhoneMap.java
+++ src/homework_13/task_10/PhoneMap.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// +
 public class PhoneMap {
     public static void main(String[] args) {
         List<Phone> phones = new ArrayList<>();
