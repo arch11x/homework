0a07e8b25bdd00a90585f1699b4700c90f701b7e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 21:28:22 +0300

    Update Task_6.java

--- src/homework_14/task_6/Task_6.java
+++ src/homework_14/task_6/Task_6.java
@@ -3,10 +3,12 @@ package homework_14.task_6;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 //"пузырьком" тоже можно же сделать?
+// пузырек - это алгоритм сортировки, а тебе нужно смержить (склеить)
 public class Task_6 {
     public static void main(String[] args) {
         int[] arrayFirst = {1, 4, 5};
         int[] arraySecond = {0, 2, 3, 6};
+        // это не оптимальное решение, ты теряешь преимущество того, что массивы изначально отсортированы
         int[] result = IntStream.concat(Arrays.stream(arrayFirst), Arrays.stream(arraySecond)).sorted().toArray();
         System.out.println(Arrays.toString(result));
     }
