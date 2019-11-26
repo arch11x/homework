bbbeaee994969f9f04e825ea61771f52d8dfca5b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 21:24:52 +0300

    Update Task_4.java

--- src/homework_14/task_4/Task_4.java
+++ src/homework_14/task_4/Task_4.java
@@ -2,4 +2,8 @@ package homework_14.task_4;
 
 public class Task_4 {
     // не совсем понимаю, как сделать  это
+    /*
+пример можешь посмотреть у 
+https://github.com/Gvan6032/Java/tree/master/hw_14/task_4
+    */
 }
